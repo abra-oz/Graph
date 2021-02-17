a40814f0810d970cf01ea35b5ed3cf0e9047ebc9
abra <55414630+abra-oz@users.noreply.github.com>
2021-02-17 11:50:00 -0600

    Add files via upload

---
+++ Assets/Point/Point Surface.shader
@@ -0,0 +1,31 @@
+﻿Shader "Graph/Point Surface" 
+{
+    Properties {
+        _Smoothness ("Smoothness", Range(0,1)) = 0.5
+    }
+
+    //SubShader {
+            
+    //}
+
+    SubShader {
+        CGPROGRAM
+        #pragma surface ConfigureSurface Standard fullforwardshadows
+        #pragma target 3.0
+
+        struct Input {
+            float3 worldPos;
+        };
+
+
+        void ConfigureSurface (Input input, inout SurfaceOutputStandard surface) {
+            surface.Albedo.rg = saturate(input.worldPos.xy * 0.5 + 0.5);
+            surface.Smoothness = _Smoothness;
+            
+        }
+
+        ENDCG
+    }
+
+    FallBack "Diffuse"
+}
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab = default;
+
+    [SerializeField, Range(10,100) ]
+    int resolution = 10;
+
+    Transform[] points;
+
+    void Awake ()
+    {
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = Instantiate(pointPrefab);
+            point.SetParent(transform, false);
+            ///point.localPosition = Vector3.right * ((i + 5f) / 5f - 1f);
+            position.x = (i + 0.5f) * step - 1f;
+            //position.y = position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            points[i] = point;
+        }
+        //point = Instantiate(pointPrefab);
+        //point.localPosition = Vector3.right * 2f;
+   
+    }
+    void Update () 
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++){
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
+        }
+    }
+}
